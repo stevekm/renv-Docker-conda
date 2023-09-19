62875698a43d02cd92386079f9a1bb7e42afb206
Stephen Kelly <stevekm@users.noreply.github.com>
2023-09-19 14:10:19 -0400

    update run script

--- run.R
+++ run.R
@@ -3,3 +3,17 @@
 library(ggplot2)
 ggplot(iris, aes(x=Sepal.Length, y=Petal.Length)) + geom_point()
 
+
+# uncomment these lines, then run
+# renv::install("data.table")
+# renv::snapshot()
+
+
+# UNCOMMENT THIS:
+# library("data.table")
+# data.table(iris)
+
+
+# then, comment out the line `library("data.table")` and run
+# renv::clean()
+# renv::snapshot()
